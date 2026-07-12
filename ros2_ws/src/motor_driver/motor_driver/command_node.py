39ea5c40bc20f21759f4e003a98f440f87bd10c4
장준환 <el06155@naver.com>
2026-07-12 13:24:57 +0900

    Add Multiple Motor Commands

--- ros2_ws/src/motor_driver/motor_driver/command_node.py
+++ ros2_ws/src/motor_driver/motor_driver/command_node.py
@@ -1,33 +1,66 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+
+
 class CommandNode(Node):
     def __init__(self):
         super().__init__('command_node')
+
         self.publisher_ = self.create_publisher(
             String,
             'motor_cmd',
             10
         )
+
+        self.commands = [
+            'forward',
+            'backward',
+            'left',
+            'right',
+            'stop'
+        ]
+
+        self.command_index = 0
+
         self.timer = self.create_timer(
             1.0,
             self.publish_command
         )
+
         self.get_logger().info(
             'Command Node Started!'
         )
+
     def publish_command(self):
         msg = String()
-        msg.data = 'forward'
+
+        msg.data = self.commands[
+            self.command_index
+        ]
+
         self.publisher_.publish(msg)
+
         self.get_logger().info(
             f'Published: {msg.data}'
         )
+
+        self.command_index += 1
+
+        if self.command_index >= len(self.commands):
+            self.command_index = 0
+
+
 def main(args=None):
     rclpy.init(args=args)
+
     node = CommandNode()
+
     rclpy.spin(node)
+
     node.destroy_node()
     rclpy.shutdown()
+
+
 if __name__ == '__main__':
     main()
--- ros2_ws/src/motor_driver/motor_driver/motor_driver_node.py
+++ ros2_ws/src/motor_driver/motor_driver/motor_driver_node.py
@@ -1,27 +1,71 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+
+
 class MotorDriverNode(Node):
     def __init__(self):
         super().__init__('motor_driver_node')
+
         self.subscription = self.create_subscription(
             String,
             'motor_cmd',
             self.motor_command_callback,
             10
         )
+
         self.get_logger().info(
             'Motor Driver Node Started!'
         )
+
     def motor_command_callback(self, msg):
+        command = msg.data
+
         self.get_logger().info(
-            f'Received motor command: {msg.data}'
+            f'Received motor command: {command}'
         )
+
+        if command == 'forward':
+            self.get_logger().info(
+                'Move Forward'
+            )
+
+        elif command == 'backward':
+            self.get_logger().info(
+                'Move Backward'
+            )
+
+        elif command == 'left':
+            self.get_logger().info(
+                'Turn Left'
+            )
+
+        elif command == 'right':
+            self.get_logger().info(
+                'Turn Right'
+            )
+
+        elif command == 'stop':
+            self.get_logger().info(
+                'Motor Stop'
+            )
+
+        else:
+            self.get_logger().warn(
+                f'Unknown Command: {command}'
+            )
+
+
 def main(args=None):
     rclpy.init(args=args)
+
     node = MotorDriverNode()
+
     rclpy.spin(node)
+
     node.destroy_node()
     rclpy.shutdown()
+
+
 if __name__ == '__main__':
     main()
